873bcbcd687ec55a0cf8f9aa62ea5884ec92cb48
Sidharth123-cpu <siddu.subbarao@gmail.com>
2023-11-25 21:49:20 -0500

    Website for demo

---
+++ bdbiproj/src/lib/components/ui/input/input.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from "svelte/elements";
+	import { cn } from "$lib/utils";
+	import type { InputEvents } from ".";
+
+	type $$Props = HTMLInputAttributes;
+	type $$Events = InputEvents;
+
+	let className: $$Props["class"] = undefined;
+	export let value: $$Props["value"] = undefined;
+	export { className as class };
+</script>
+
+<input
+  class={cn(
+    "flex h-10 w-full rounded-md border-4 border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-white focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 text-white",
+    className
+  )}
+  bind:value
+  on:blur
+  on:change
+  on:click
+  on:focus
+  on:keydown
+  on:keypress
+  on:keyup
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+  on:paste
+  on:input
+  style="outline: none;" 
+  {...$$restProps}
+/>
+
+
+<style>
+	input::placeholder {
+	  text-align: center;
+	}
+  </style>
+
+
+
