a8f0bd1f03a0c2488a8f612e255cce08217898d1
Mcginnis8 <camcginnis19@gmail.com>
2023-11-27 22:20:25 -0500

    added map

--- bdbiproj/src/routes/+page.svelte
+++ bdbiproj/src/routes/+page.svelte
@@ -50,7 +50,7 @@
           width="600"
           height="450"
           style="border:0; margin-top: 20px;"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCPl0t1z471mvNMji79lGnQ7FbSzrkuXQY&q=Space+Needle,Seattle+WA">
+        >
         </iframe>
       </div>
     </Tabs.Content>
